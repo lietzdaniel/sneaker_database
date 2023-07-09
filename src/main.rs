f22ad5964aa88f15d230da37ea07c91845455409
Daniel Lietz <ge96jek@mytum.de>
2023-07-09 19:02:32 +0200

    database, hashmap return, first functionalities

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,30 @@
 mod scrape;
+use rusqlite::{Connection, Result};
+
 
 #[tokio::main]
-async fn main() {
+async fn main() ->Result<()> {
+    let conn = Connection::open("shoes.db")?;
+    conn.execute( "CREATE TABLE IF NOT EXISTS shoes (
+        id  INTEGER PRIMARY KEY AUTOINCREMENT,
+        style_id   TEXT NOT NULL,
+        name TEXT NOT NULL,
+        type TEXT NOT NULL,
+        model TEXT NOT NULL,
+        colorway TEXT NOT NULL,
+        description TEXT NOT NULL,
+        extras TEXT,
+        retail_price FLOAT,
+        release_date TIMESTAMP NOT NULL,
+        image TEXT NOT NULL
+    );", [])?;
+    conn.execute( "CREATE TABLE IF NOT EXISTS shoe_prices (
+        id  INTEGER PRIMARY KEY AUTOINCREMENT,
+        style_id   TEXT NOT NULL,
+        size TEXT NOT NULL,
+        price TEXT,
+        timestamp TIMESTAMP NOT NULL
+    );", [])?;
     loop {
         println!("Hello, this is your shoe database manager. What would you like to do today?");
         println!("[1]   Search for a shoe to add to the database");
@@ -29,10 +52,12 @@ async fn main() {
             }
         }
     }
+    Ok(())
 }
 
 pub async fn add_shoe() -> () {
-    loop {
+    'mainloop: loop {
+        println!("/*--------------------------------------------------ADDING SHOE--------------------------------------------------*/");
         println!("Which shoe do you want to search for?");
         let mut input_line = String::new();
         std::io::stdin()
@@ -40,24 +65,46 @@ pub async fn add_shoe() -> () {
             .expect("Failed to read line");
         println!("Please select the model you want to add");
         let (link_vec, shoe_vec) = scrape::scrape(&input_line).await;
-        for (idx, shoe) in shoe_vec.iter().enumerate() {
-            println!("[{}]  {}", idx + 1, shoe);
+        let mut iterator = shoe_vec.iter().cloned();
+        let mut idx = 1;
+        let mut show_more = true;
+        println!("[x]  Exit to Main Menu");
+        println!("[m]  Show 5 more shoes");
+        'showmore: loop {
+            if show_more {
+            for  shoe in iterator.by_ref().take(5) {
+                println!("[{}]  {}", idx, shoe);
+                idx += 1;
+            }
+            show_more = false;
         }
 
-        let mut input_int = String::new();
-        std::io::stdin()
-            .read_line(&mut input_int)
-            .expect("Failed to read line");
-        match input_int.trim().parse::<u8>() {
-            Ok(number) => {
-                if number > link_vec.len() as u8 || number == 0 {
-                    continue;
+            let mut input_int = String::new();
+            std::io::stdin()
+                .read_line(&mut input_int)
+                .expect("Failed to read line");
+          
+            let mut input_str =input_int.clone();
+           
+            match input_int.trim().parse::<u8>() {
+                Ok(number) => {
+                    if number > link_vec.len() as u8 || number == 0 {
+                        continue;
+                    }
+                    scrape::get_shoe_info(&link_vec[number as usize]).await;
+
+                    break 'mainloop
                 }
-                scrape::get_shoe_info(&link_vec[number as usize]).await;
-            }
-            Err(_) => {
-                println!("Invalid input. Please try again.");
-                continue; // Repeat the loop if the input is not a valid integer
+                Err(e) => {
+                 
+                    if input_str.len() == 2 && input_str.chars().nth(0).unwrap() == 'm' {
+                       
+                        show_more = true;
+                        continue 'showmore;
+                    }
+                    println!("Invalid input. Please try again.");
+                    continue; // Repeat the loop if the input is not a valid integer
+                }
             }
         }
     }
--- src/scrape.rs
+++ src/scrape.rs
@@ -1,5 +1,5 @@
 use http::{HeaderMap, HeaderValue};
-use std::{collections::HashMap, vec};
+use std::{collections::HashMap, vec, hash::Hash};
 pub async fn scrape(shoe_name: &str) -> (Vec<String>, Vec<String>) {
     let query_shoe_name = shoe_name.replace(" ", "+");
 
@@ -42,24 +42,24 @@ pub async fn handle_grid_response(response: &String) -> (Vec<String>, Vec<String
     let document = scraper::Html::parse_document(response);
     let selector = scraper::Selector::parse("a[data-testid=RouterSwitcherLink]").unwrap();
 
-    for element in document.select(&selector).take(5) {
+    for element in document.select(&selector) {
         if let Some(href) = element.value().attr("href") {
             shoe_vec.push(href.to_string());
         }
     }
     let selector = scraper::Selector::parse("p.chakra-text.css-3lpefb").unwrap();
-    for element in document.select(&selector).take(5) {
-        println!("{}", element.text().collect::<String>());
+    for element in document.select(&selector) {
+       
         link_vec.push(element.text().collect::<String>());
     }
     (shoe_vec, link_vec)
 }
 
-pub async fn get_shoe_info(links: &String) -> Vec<String> {
+pub async fn get_shoe_info(links: &String) -> HashMap<String, String> {
     let mut query_shoe_url: String = String::from("https://stockx.com");
 
     query_shoe_url.push_str(links);
-    println!("{}", query_shoe_url);
+   
     let mut headers = HeaderMap::new();
     headers.insert("User-Agent", HeaderValue::from_str("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36").unwrap());
     headers.insert("accept", HeaderValue::from_str("application/json").unwrap());
@@ -80,31 +80,64 @@ pub async fn get_shoe_info(links: &String) -> Vec<String> {
         Ok(s) => return handle_shoe_response(&s.text().await.unwrap()).await,
         Err(e) => {
             println!("{e}");
-            return ([].to_vec());
+            return HashMap::new();
         }
     }
 }
 
-pub async fn handle_shoe_response(response: &String) -> (Vec<String>) {
+pub async fn handle_shoe_response(response: &String) -> HashMap<String,String> {
     let mut info_vec: Vec<String> = Vec::new();
+    let mut shoe_info = HashMap::new();
 
     let document = scraper::Html::parse_document(response);
+    let name_selector = scraper::Selector::parse("p.chakra-text.css-exht5z",).unwrap();
+    let description_selector = scraper::Selector::parse("p.chakra-text.css-16vde4f",).unwrap();
+    let data_selector = scraper::Selector::parse("p.chakra-text.css-wgsjnl",).unwrap();
+    let type_selector = scraper::Selector::parse(  "h1.chakra-heading.css-t7k2e1[data-component=primary-product-title]",).unwrap();
+    let model_selector =scraper::Selector::parse("span[data-component=secondary-product-title]").unwrap();
+   
+    let image_selector = scraper::Selector::parse("img.chakra-image.css-g98gbd").unwrap();
+    
+    let description_component = document.select(&description_selector).next().unwrap();
+    let type_component = document.select(&type_selector).next().unwrap();
+    let model_component = document.select(&model_selector).next().unwrap();
+    
+    let name_component = document.select(&name_selector).next().unwrap();
+    let image_component =  document.select(&image_selector).next().unwrap(); 
+    let keys = ["style-id","colorway","retail_price","release_date","extras"];
+    for (idx,element) in  document.select(&data_selector).enumerate() {
+        if idx >= keys.len() {
+            shoe_info.insert("extras".to_string(),"".to_string()); //Extras is optional, so it may not be found
+            break;
+        }
+            shoe_info.insert(keys[idx].to_string(), element.text().collect::<String>());
+        
+    }
 
-    let primary_selector = scraper::Selector::parse(
-        "h1.chakra-heading.css-t7k2e1[data-component=primary-product-title]",
-    )
-    .unwrap();
-    let secondary_selector =
-        scraper::Selector::parse("span[data-component=secondary-product-title]").unwrap();
+    let description_text = description_component.text().collect::<String>();
+    let name_text = name_component.text().collect::<String>();
+    let mut type_text = type_component.text().collect::<String>();
+    let model_text = model_component.text().collect::<String>();
+    let image_link = image_component.value().attr("src").unwrap().to_string();
+    type_text.truncate(type_text.len()-model_text.len());
+   
 
-    let primary_component = document.select(&primary_selector).next().unwrap();
-    let secondary_component = document.select(&secondary_selector).next().unwrap();
 
-    let primary_text = primary_component.text().collect::<String>();
-    let secondary_text = secondary_component.text().collect::<String>();
+   shoe_info.insert("description".to_string(), description_text);
+   shoe_info.insert("name".to_string(), name_text);
+   shoe_info.insert("type".to_string(), type_text);
+   shoe_info.insert("model".to_string(), model_text);
+    shoe_info.insert("image".to_string(), image_link);
+    println!("{shoe_info:?}");
+   
+ 
 
-    println!("Primary Product Component: {}", primary_text);
-    println!("Secondary Product Component: {}", secondary_text);
 
-    [].to_vec()
+    
+   
+    
+
+   
+   
+    shoe_info
 }
